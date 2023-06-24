8df2002a7addd8c838e4323cd3601e45e5d470bf
sunilkushwaha <sunilkushwaha055>
2023-06-24 21:08:44 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,41 @@ import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-
-  const [firstR, setFirstR] = useState(0)
-  const [secRange, setSecRange]= useState(50)
-  const [third, setThird] = useState(50)
+  const [firstR, setFirstR] = useState(0);
+  const [secRange, setSecRange] = useState(50);
+  const [third, setThird] = useState(50);
   return (
-    <>
+    <div>
       <div id="max-sum-holder">
-        Input max sum:- 
-        <input type="number" value={third} onChange={e=> setThird(e.target.value)}></input>
+        Input max sum:-
+        <input
+          type={"number"}
+          value={third}
+          onChange={(e) => setThird(e.target.value)}
+        ></input>
       </div>
       <div id="range-a-holder">
-        <input type="range" value={firstR} onChange={(e)=> setFirstR(e.target.value)} max={third - secRange}></input>
-        <span>{firstR}</span>
+        <input
+          type={"range"}
+          value={firstR}
+          onChange={(e) => setFirstR(e.target.value)}
+          max={third - secRange}
+        />
+        <span id="range-a-value">{firstR}</span>
       </div>
-        <div id="range-b-holder">
-          <input type="range" value={secRange} onChange={(e)=> setSecRange(e.target.value)} max={third-firstR}></input>
-          <span>{secRange}</span>
-        </div>
-        <div id="sum">
-          {[firstR, secRange].map(Number).reduce((sum, cur)=> sum+ cur, 0)}
-        </div>
-    </>
+      <div id="range-b-holder">
+        <input
+          type={"range"}
+          value={secRange}
+          onChange={(e) => setSecRange(e.target.value)}
+          max={third - firstR}
+        />
+        <span id="range-b-value">{secRange}</span>
+      </div>
+      <div id="sum">
+        {[firstR, secRange].map(Number).reduce((sum, cur) => sum + cur, 0)}
+      </div>
+    </div>
   );
 };
 
